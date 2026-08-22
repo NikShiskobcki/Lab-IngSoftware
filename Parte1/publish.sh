b1a8241c615d2bcde69e2185b86fcdfd2080cf9d
NikShiskobcki <87676940+NikShiskobcki@users.noreply.github.com>
2026-08-21 23:31:31 -0300

    Parte 1 laboratorio Ingenieria de Software

---
+++ Parte1/publish.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mosquitto_pub -h localhost -p 1883 -t turno -m '{"status":"turno_creado","fechaHora":"2026-09-01T10:15:00","turno":{"id":35,"email_cliente":"a@a.com","telefono_cliente":"11111111","idPersonal":8,"fecha":"2026-09-15","hora":"14:30"}}'
